da2ff90e31268bd5b6e90cf2ebee2cdea5a3b936
mirage335 <spamfreemirage335@gmail.com>
2018-05-25 17:22:10 -0400

    Preferences management.

--- _prog/core.sh
+++ _prog/core.sh
@@ -1,9 +1,39 @@
-#all2=""
+#Arduino may ignore "--pref" parameters, possibly due to bugs present in some versions. Consequently, it is possible some stored perferences may interfere with normal script operation. As a precaution, these are deleted.
+_arduino_deconfigure_sequence() {
+	_start
+	
+	local arduinoPreferences
+	arduinoPreferences="$HOME"/.arduino15/preferences.txt
+	
+	mv "$arduinoPreferences" "$safeTmp"/preferences.txt
+	
+	grep -v '^sketchbook\.path' "$safeTmp"/preferences.txt > "$safeTmp"/intermediate
+	mv "$safeTmp"/intermediate "$safeTmp"/preferences.txt
+	
+	grep -v '^last\.' "$safeTmp"/preferences.txt > "$safeTmp"/intermediate
+	mv "$safeTmp"/intermediate "$safeTmp"/preferences.txt
+	
+	mv "$safeTmp"/preferences.txt "$arduinoPreferences"
+	
+	_stop
+}
+
+_arduino_deconfigure() {
+	"$scriptAbsoluteLocation" _arduino_deconfigure_sequence "$@"
+}
 
 #command
 _arduino_command() {
-	export _JAVA_OPTIONS=-Duser.home="$HOME" "$_JAVA_OPTIONS"
-	"$scriptAbsoluteFolder"/_local/arduino-1.8.5/arduino "$@"
+	export _JAVA_OPTIONS=-Duser.home="$HOME"' '"$_JAVA_OPTIONS"
+	
+	local arduinoExecutable
+	arduinoExecutable="$scriptAbsoluteFolder"/_local/arduino-1.8.5/arduino
+	
+	"$arduinoExecutable" --save-prefs --pref sketchbook.path="$HOME"/Arduino "$@"
+	
+	"$arduinoExecutable" "$@"
+	
+	_arduino_deconfigure
 }
 
 #edit
