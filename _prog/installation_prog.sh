543dcb7cf7dbe77c889665dcdc99252e690f49ed
mirage335 <spamfreemirage335@gmail.com>
2018-05-29 19:25:53 -0400

    Draft package creation.

--- _prog/installation_prog.sh
+++ _prog/installation_prog.sh
@@ -1,22 +1,38 @@
 _check_prog() {
 	! _typeDep java && return 1
 	
+	[[ -e "$au_openocdStaticUB" ]] && ! "$au_openocdStaticUB" _test_prog "$@" && return 1
+	
 	return 0
 }
 
 _test_prog() {
 	_getDep java
 	
+	[[ -e "$au_openocdStaticUB" ]] && ! "$au_openocdStaticUB" _test_prog "$@" && _stop 1
+	
 	! _check_prog && echo 'missing: dependency mismatch' && _stop 1
 }
 
-_setup_udev() {
-	! _wantSudo && echo 'denied: sudo' && _stop 1
+_setup_prog() {
+	#true
 	
-	sudo -n cp "$scriptLocal"/98-openocd.rules /etc/udev/rules.d/
-	sudo -n usermod -a -G plugdev "$USER"
+	[[ -e "$au_openocdStaticUB" ]] && "$au_openocdStaticUB" _setup_prog "$@"
 }
 
-_setup_prog() {
-	_setup_udev
+_package_prog() {
+	cp -a "$scriptAbsoluteFolder"/.git "$safeTmp"/package/
+	
+	cp -a "$au_arduinoInstallation" "$safeTmp"/package/
+	
+	mkdir -p "$safeTmp"/package/arduino
+	cp -a "$au_arduinoInstallation"/. "$safeTmp"/package/arduino
+	
+	rm "$safeTmp"/package/arduino/portable
+	mkdir -p "$safeTmp"/package/arduino/portable
+	cp -a "$globalFakeHome"/.arduino15/. "$safeTmp"/package/arduino/portable/
+	
+	rm "$safeTmp"/package/arduino/portable/sketchbook
+	mkdir -p "$safeTmp"/package/arduino/portable/sketchbook
+	cp -a "$globalFakeHome"/Arduino/. "$safeTmp"/package/arduino/portable/sketchbook
 }
--- _prog/specglobalvars.sh
+++ _prog/specglobalvars.sh
@@ -1,1 +1,2 @@
 export au_arduinoInstallation="$globalFakeHome"/arduino-1.8.5
+export au_openocdStaticUB="$scriptLib"/openocd-static/ubiquitous_bash.sh
