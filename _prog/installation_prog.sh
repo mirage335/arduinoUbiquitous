7ee92cb8612a8a655fd84b50f0bbaa2ed49e0a73
mirage335 <spamfreemirage335@gmail.com>
2020-09-20 09:08:41 -0400

    Documentation. Improved permissions setup. Submodule.

--- _prog/installation_prog.sh
+++ _prog/installation_prog.sh
@@ -12,10 +12,26 @@ _test_prog() {
 	return 0
 }
 
+# CAUTION: Reboot or relogin may be required to take effect.
+_setup_udev() {
+	! _wantSudo && echo 'denied: sudo' && _stop 1
+	
+	sudo -n bash -c '[[ -e /etc/udev/rules.d/ ]]' && sudo -n cp -r "$scriptLib"/app/udev-avr/rules/. /etc/udev/rules.d/
+	
+	# WARNING: System groups are expected to have been created with correct uid/gid by other utilities prior to running these scripts.
+	#sudo -n groupadd plugdev
+	sudo -n usermod -a -G plugdev "$USER"
+	
+	#sudo -n groupadd dialout
+	sudo -n usermod -a -G dialout "$USER"
+}
+
 _setup_prog() {
 	#true
 	
 	! _setup_prog_arduino "$@" && _stop 1
+	
+	_setup_udev
 	
 	return 0
 }
